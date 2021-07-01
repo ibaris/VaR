0d6ba586aecaf35f65045a8a24e3b390f1ea3f37
bari_is <ismail.baris@dlr.de>
2021-07-01 18:10:00 +0200

    feat(var.methods.cdar): Add new metric `Conditional Drawdown at Risk` (CDaR)

--- var/methods.py
+++ var/methods.py
@@ -11,12 +11,12 @@ import math
 import warnings
 
 import numpy as np
+import pandas as pd
 from arch import arch_model
 from scipy.stats import gumbel_r
 from scipy.stats import norm
-import pandas as pd
 
-__all__ = ["historic", "parametric", "monte_carlo", "garch"]
+__all__ = ["historic", "parametric", "monte_carlo", "garch", "cdar"]
 
 
 def historic(daily_pnl, alpha):
@@ -28,8 +28,8 @@ def historic(daily_pnl, alpha):
     ----------
     daily_pnl : pd.DataFrame
         A DataFrame with the daily profit and losses.
-    alpha : list or None
-        A list confidence intervals (alpha values) for VaR. If None, the default values are [5%, 2.5%, 1%].
+    alpha : list
+        A list confidence intervals (alpha values) for VaR.
 
     Returns
     -------
@@ -57,8 +57,8 @@ def parametric(daily_pnl, alpha, daily_std):
     ----------
     daily_pnl : pd.DataFrame
         A DataFrame with the daily profit and losses.
-    alpha : list or None
-        A list confidence intervals (alpha values) for VaR. If None, the default values are [5%, 2.5%, 1%].
+    alpha : list
+        A list confidence intervals (alpha values) for VaR.
     daily_std : float
         Daily Standard Deviation of the portfolio.
 
@@ -94,8 +94,8 @@ def monte_carlo(daily_pnl, alpha, stressed=False):
     ---------
     daily_pnl : pd.DataFrame
         A DataFrame with the daily profit and losses.
-    alpha : list or None
-        A list confidence intervals (alpha values) for VaR. If None, the default values are [5%, 2.5%, 1%].
+    alpha : list
+        A list confidence intervals (alpha values) for VaR.
     stressed : bool
         Use the Stressed Monte Carlo Method. Default is False.
 
@@ -112,7 +112,7 @@ def monte_carlo(daily_pnl, alpha, stressed=False):
     """
 
     if not stressed:
-        PnL_list = np.random.normal(np.mean(daily_pnl.values), np.std(daily_pnl.values), 500000)
+        PnL_list = np.random.normal(np.mean(daily_pnl.values), np.std(daily_pnl.values), 100000)
     else:
         block_maxima = daily_pnl.resample('W').min().dropna()
         loc, scale = gumbel_r.fit(block_maxima)
@@ -135,8 +135,8 @@ def garch(daily_pnl, alpha):
     ----------
     daily_pnl : pd.DataFrame
         A DataFrame with the daily profit and losses.
-    alpha : list or None
-        A list confidence intervals (alpha values) for VaR. If None, the default values are [5%, 2.5%, 1%].
+    alpha : list
+        A list confidence intervals (alpha values) for VaR.
 
     Returns
     -------
@@ -154,13 +154,71 @@ def garch(daily_pnl, alpha):
         res = am.fit(disp='off')
         forecasts = res.forecast(reindex=True)
 
-    q = am.distribution.ppf(alpha / 100, res.params[-2:])
+    q = am.distribution.ppf(alpha, res.params[-2:])
     value_at_risk = forecasts.mean.values + np.sqrt(forecasts.variance).values * q[None, :]
 
-    var_values = [value_at_risk[[-1, ]][0][0], value_at_risk[[-1, ]][0][1], value_at_risk[[-1, ]][0][2]]
+    var_values = [float(value_at_risk[[-1, ]][0][0]), float(value_at_risk[[-1, ]][0][1]),
+                  float(value_at_risk[[-1, ]][0][2])]
+
     cvar_values = [np.mean(daily_pnl[daily_pnl <= item]) for item in var_values]
     cvar_values = [var_values[-1] if math.isnan(item) else item for item in cvar_values]
 
     data = np.append(var_values, cvar_values).flatten()
 
     return data
+
+
+def cdar(daily_pnl, alpha, comp=True):
+    """
+    Calculate the Conditional Drawdown at Risk (CDaR) of a returns series.
+
+    Parameters
+    ----------
+    daily_pnl : pd.DataFrame
+        A DataFrame with the daily profit and losses.
+    alpha : list
+        A list confidence intervals (alpha values) for VaR.
+    comp : bool
+        If True (Default) use the compounded cumulative returns.
+
+    Returns
+    -------
+    out : list
+        A list object with Value at Risk values at different confidence intervals.
+    """
+    a = np.array(daily_pnl, ndmin=2)
+    if comp:
+        prices = 1 + np.insert(np.array(a), 0, 0, axis=0)
+        NAV = np.cumprod(prices, axis=0)
+        DD = list()
+
+        peak = -99999
+        for i in NAV:
+            peak = i if i > peak else peak
+            DD.append(-(peak - i) / peak)
+        del DD[0]
+    else:
+        prices = np.insert(np.array(a), 0, 1, axis=0)
+        NAV = np.cumsum(np.array(prices), axis=0)
+        DD = []
+
+        peak = -99999
+        for i in NAV:
+            peak = i if i > peak else peak
+            DD.append(-(peak - i))
+        del DD[0]
+
+    sorted_DD = np.sort(np.array(DD), axis=0)
+    indices = [int(np.ceil(item * len(sorted_DD)) - 1) for item in alpha]
+
+    value = list()
+    for i, index in enumerate(indices):
+        sum_var = 0
+        for j in range(index + 1):
+            sum_var = sum_var + sorted_DD[j] - sorted_DD[index]
+        value_tmp = -sorted_DD[index] - sum_var / (alpha[i] * len(sorted_DD))
+        value.append(np.array(value_tmp).item())
+
+    value.extend(value)
+
+    return value
